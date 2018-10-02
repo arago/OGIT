205fa04b4a344acaf4ae9bcd27a862eb73268dc9
Jenkins CK <devops@arago.co>
2018-10-02 08:40:37 +0000

    build

--- group__Auth__attributes.js
+++ group__Auth__attributes.js
@@ -1,4 +1,5 @@
 var group__Auth__attributes =
 [
-    [ "_iam-domain", "group__httpwwwpurlorgogitAuth__iam-domain.html", null ]
+    [ "_iam-domain", "group__httpwwwpurlorgogitAuth__iam-domain.html", null ],
+    [ "allowedTypes", "group__httpwwwpurlorgogitAuthallowedTypes.html", null ]
 ];
